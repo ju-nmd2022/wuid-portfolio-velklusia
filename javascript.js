53b9305cf880b70cc15a73945bd87cc0e2798dd2
velklusia <116886960+velklusia@users.noreply.github.com>
2022-11-15 22:08:35 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,23 @@
 (/It initializes animations on my website/)
 AOS.init();
+
+//Portfolio Slider
+
+$('.portfolio-slider') .owlCarousel (({
+  loop: true,
+  nav: true,
+  dots: true,
+  smartSpeed: 1000,
+  navText: ["<i class="ri-arrow-up-line"></i>],
+            responsive; {
+            0:{
+            items: 1
+            },
+            600:{
+            items:2
+            },
+            992:{
+            items:3
+                                     }
+}
+}
